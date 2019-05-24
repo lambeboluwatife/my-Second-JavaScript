2ab2e2516f506fd206592678bc613c86c699aab3
Lambe Boluwatife <40843759+lambeboluwatife@users.noreply.github.com>
2019-05-24 11:18:23 +0100

    Add files via upload

---
+++ js2.js
@@ -0,0 +1,432 @@
+let staff = [
+  {
+    FirstName: "Vanessa",
+    SecondName: "Rogers",
+    Gender: "Female",
+    Age: 19,
+    Salary: 60193,
+  },
+  {
+    FirstName: "Charlotte",
+    SecondName: "Hunt",
+    Gender: "Female",
+    Age: 24,
+    Salary: 92383,
+  },
+  {
+    FirstName: "Marcus",
+    SecondName: "Foster",
+    Gender: "Male",
+    Age: 21,
+    Salary: 65760,
+  },
+  {
+    FirstName: "Myra",
+    SecondName: "Brooks",
+    Gender: "Female",
+    Age: 21,
+    Salary: 174036,
+  },
+  {
+    FirstName: "Caroline",
+    SecondName: "Armstrong",
+    Gender: "Female",
+    Age: 29,
+    Salary: 149879,
+  },
+  {
+    FirstName: "Belinda",
+    SecondName: "Carroll",
+    Gender: "Female",
+    Age: 19,
+    Salary: 188935,
+  },
+  {
+    FirstName: "Olivia",
+    SecondName: "Armstrong",
+    Gender: "Female",
+    Age: 26,
+    Salary: 34782,
+  },
+  {
+    FirstName: "April",
+    SecondName: "Cooper",
+    Gender: "Female",
+    Age: 20,
+    Salary: 68263,
+  },
+  {
+    FirstName: "Edith",
+    SecondName: "Harrison",
+    Gender: "Female",
+    Age: 23,
+    Salary: 54709,
+  },
+  {
+    FirstName: "Vivian",
+    SecondName: "Henderson",
+    Gender: "Female",
+    Age: 20,
+    Salary: 155709,
+  },
+  {
+    FirstName: "Ashton",
+    SecondName: "Martin",
+    Gender: "Male",
+    Age: 27,
+    Salary: 166647,
+  },
+  {
+    FirstName: "Gianna",
+    SecondName: "Moore",
+    Gender: "Female",
+    Age: 23,
+    Salary: 191619,
+  },
+  {
+    FirstName: "Camila",
+    SecondName: "Allen",
+    Gender: "Female",
+    Age: 23,
+    Salary: 85567,
+  },
+  {
+    FirstName: "Gianna",
+    SecondName: "Morrison",
+    Gender: "Female",
+    Age: 28,
+    Salary: 191509,
+  },
+  {
+    FirstName: "Paul",
+    SecondName: "Cooper",
+    Gender: "Male",
+    Age: 28,
+    Salary: 39471,
+  },
+  {
+    FirstName: "Tara",
+    SecondName: "Mason",
+    Gender: "Female",
+    Age: 19,
+    Salary: 189780,
+  },
+  {
+    FirstName: "Aida",
+    SecondName: "Tucker",
+    Gender: "Female",
+    Age: 21,
+    Salary: 143187,
+  },
+  {
+    FirstName: "Richard",
+    SecondName: "Cameron",
+    Gender: "Male",
+    Age: 21,
+    Salary: 116644,
+  },
+  {
+    FirstName: "Amy",
+    SecondName: "Stewart",
+    Gender: "Female",
+    Age: 29,
+    Salary: 126961,
+  },
+  {
+    FirstName: "Elenor",
+    SecondName: "Thompson",
+    Gender: "Female",
+    Age: 22,
+    Salary: 159003,
+  },
+  {
+    FirstName: "Alberta",
+    SecondName: "Edwards",
+    Gender: "Female",
+    Age: 21,
+    Salary: 105648,
+  },
+  {
+    FirstName: "Gianna",
+    SecondName: "Evans",
+    Gender: "Female",
+    Age: 27,
+    Salary: 183805,
+  },
+  {
+    FirstName: "Florrie",
+    SecondName: "Kelley",
+    Gender: "Female",
+    Age: 24,
+    Salary: 180568,
+  },
+  {
+    FirstName: "Miranda",
+    SecondName: "Harris",
+    Gender: "Female",
+    Age: 19,
+    Salary: 185753,
+  },
+  {
+    FirstName: "Henry",
+    SecondName: "Davis",
+    Gender: "Male",
+    Age: 18,
+    Salary: 83804,
+  },
+  {
+    FirstName: "Kirsten",
+    SecondName: "Barnes",
+    Gender: "Female",
+    Age: 26,
+    Salary: 161833,
+  },
+  {
+    FirstName: "Jack",
+    SecondName: "Chapman",
+    Gender: "Male",
+    Age: 19,
+    Salary: 41007,
+  },
+  {
+    FirstName: "Myra",
+    SecondName: "Kelly",
+    Gender: "Female",
+    Age: 27,
+    Salary: 118346,
+  },
+  {
+    FirstName: "Emma",
+    SecondName: "Evans",
+    Gender: "Female",
+    Age: 23,
+    Salary: 126125,
+  },
+  {
+    FirstName: "Blake",
+    SecondName: "Evans",
+    Gender: "Male",
+    Age: 24,
+    Salary: 69702,
+  },
+  {
+    FirstName: "Aldus",
+    SecondName: "Armstrong",
+    Gender: "Male",
+    Age: 26,
+    Salary: 132491,
+  },
+  {
+    FirstName: "Adelaide",
+    SecondName: "Martin",
+    Gender: "Female",
+    Age: 20,
+    Salary: 175263,
+  },
+  {
+    FirstName: "Victoria",
+    SecondName: "Elliott",
+    Gender: "Female",
+    Age: 21,
+    Salary: 111645,
+  },
+  {
+    FirstName: "Nicole",
+    SecondName: "Harrison",
+    Gender: "Female",
+    Age: 22,
+    Salary: 54624,
+  },
+  {
+    FirstName: "Edwin",
+    SecondName: "Hamilton",
+    Gender: "Male",
+    Age: 18,
+    Salary: 152586,
+  },
+  {
+    FirstName: "Jessica",
+    SecondName: "Robinson",
+    Gender: "Female",
+    Age: 29,
+    Salary: 85446,
+  },
+  {
+    FirstName: "Sofia",
+    SecondName: "Turner",
+    Gender: "Female",
+    Age: 20,
+    Salary: 131564,
+  },
+  {
+    FirstName: "Cadie",
+    SecondName: "Higgins",
+    Gender: "Female",
+    Age: 21,
+    Salary: 138663,
+  },
+  {
+    FirstName: "Jessica",
+    SecondName: "Turner",
+    Gender: "Female",
+    Age: 24,
+    Salary: 91319,
+  },
+  {
+    FirstName: "Walter",
+    SecondName: "Hamilton",
+    Gender: "Male",
+    Age: 25,
+    Salary: 117408,
+  },
+  {
+    FirstName: "Alexander",
+    SecondName: "Farrell",
+    Gender: "Male",
+    Age: 26,
+    Salary: 65878,
+  },
+  {
+    FirstName: "Justin",
+    SecondName: "Howard",
+    Gender: "Male",
+    Age: 21,
+    Salary: 165575,
+  },
+  {
+    FirstName: "Chelsea",
+    SecondName: "Holmes",
+    Gender: "Female",
+    Age: 21,
+    Salary: 32690,
+  },
+  {
+    FirstName: "Alberta",
+    SecondName: "Wilson",
+    Gender: "Female",
+    Age: 22,
+    Salary: 143459,
+  },
+  {
+    FirstName: "Miller",
+    SecondName: "Murphy",
+    Gender: "Male",
+    Age: 25,
+    Salary: 163775,
+  },
+  {
+    FirstName: "Adrianna",
+    SecondName: "Hawkins",
+    Gender: "Female",
+    Age: 29,
+    Salary: 64412,
+  },
+  {
+    FirstName: "Dominik",
+    SecondName: "Reed",
+    Gender: "Male",
+    Age: 23,
+    Salary: 42643,
+  },
+  {
+    FirstName: "Gianna",
+    SecondName: "Craig",
+    Gender: "Female",
+    Age: 18,
+    Salary: 141944,
+  },
+  {
+    FirstName: "Alissa",
+    SecondName: "Cameron",
+    Gender: "Female",
+    Age: 25,
+    Salary: 101581,
+  },
+  {
+    FirstName: "Aldus",
+    SecondName: "Adams",
+    Gender: "Male",
+    Age: 28,
+    Salary: 132894,
+  },
+]
+
+
+/*Use for loop to iterate over the array of objects
+Print to the console the firstname and lastname of the all workers using forEach()
+Print to the console the total salaries of all workers using reduce()
+Print to the console the average age of all workers using reduce()
+Print to the console the array of workers that are above the age of 30 using filter()
+The income of the company was trippled so the management decided to double the salaries
+of each workers. You are to print to the console the new salaries of the workers using map()
+and forEach() (use the chaining method).'*/
+
+// firstname and lastname
+// Object.keys(staff).forEach(key => console.log(staff[key.FirstName]))
+for (let i = 0; i < staff.length; i++) {
+  console.log(`${staff[i].FirstName} ${staff[i].SecondName}`);
+}
+
+
+
+// Total Salaries
+const totalSalaries = staff.reduce((result, item) => result + item.Salary, 0)
+console.log(totalSalaries);
+
+
+//Average age
+const averageAge = staff.reduce((acc, curr) => acc + curr.Age, 0)
+console.log(averageAge);
+
+
+//Age Filter
+const great = staff.filter(age => age.Age > 20)
+console.log(great);
+
+
+/*Create a function that takes in an array of numbers as a parameter and returns the first and last element of the array.
+Use arrow function
+Create a functiion that accepts an object as a parameter and returns a sentence using the object values.
+For example, if this is the object
+{ firstName:'Vanessa',
+secondName: 'Rogers',
+gender: 'Female',
+age: 19,
+salary: 60193}
+
+My return statement should be Your name is Vanessa Rogers, you are a female,
+you are a female and your current salary is $60193.
+
+In one line, create a function that takes in two parameters, name and score.
+The function should return the name and score. E.g Hello, Teni, your score is 500*/
+
+//Function 1 (a)
+function array(marks) {
+  return `${marks[0]} and ${marks[6]}`
+}
+console.log(array([1, 2, 3, 4, 5, 6, 7]));
+
+//Function 1 (b)
+const arrays = (marks) => {
+  return `${marks[0]} and ${marks[6]}`;
+}
+console.log(arrays([1, 2, 3, 4, 5, 6, 7]));
+
+// //Function 2
+// const details = (staff) => {
+//   return `  My name is ${staff.name} ${staff.surname}, am a ${staff.gender},
+//   am ${staff.age} years old and my current pay is ${staff.salary}.`;
+// }
+// console.log(details(  { name: 'Boluwatife',
+// surname: 'Lambe',
+// gender: 'Male',
+// age: 19,
+// salary: 200193,
+// }));
+//
+// //Function 3
+// //In one line, create a function that takes in two parameters, name and score.
+// //The function should return the name and score. E.g Hello, Teni, your score is 500
+//
+// const line = (name, score) => `Hello, ${name}, your score is ${score}.`
+// console.log(line("Boluwatife", 239));
